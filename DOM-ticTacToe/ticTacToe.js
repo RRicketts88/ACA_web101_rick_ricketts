6b53baa5e22a42bd18efe3d8d21a49e6b97805b5
RRicketts88 <rricketts88@gmail.com>
2019-02-26 20:49:36 -0600

    DOM-ticTacToe

---
+++ DOM-ticTacToe/ticTacToe.js
@@ -0,0 +1,10 @@
+console.log("Your javascript is working!")
+
+let market = "X"
+
+function (x) {
+    document.getElementById(x).innerHTML='marker';
+    if(market ="x") {
+        console.log(marker)
+    }
+  }
