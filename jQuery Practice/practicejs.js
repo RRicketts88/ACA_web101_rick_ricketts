ada1dc1adf4925cd8e50d4d5c07c7bd60d7eaf9f
RRicketts88 <rricketts88@gmail.com>
2019-03-07 20:42:45 -0600

    jQuery Practice

--- jQuery Practice/practicejs.js
+++ jQuery Practice/practicejs.js
@@ -1,8 +1,11 @@
+// once the page is ready, it will run a function, starting with no value in the block
 $(document).ready(function() {
   let $block = null;
-  
+
+  // if any of the rows are clicked on, it will run this function
   $('[data-row]').click(function() {
     if ($block) {
+      // below we either do nothing or move the block to another row if an appropriate block is clicked on
       $(this).append($block);
       $block = null;
     } else {
